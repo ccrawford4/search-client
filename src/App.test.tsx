1e50917b13a9cbdd60ce4f57de094ae6d08eae86
ccrawford4 <calumcrawford9@gmail.com>
2024-09-29 22:27:21 -0700

    fixed test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,7 @@
-test('equals true!', () => {
-  expect(true).toEqual(true);
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+it('should render the correct test', async () => {
+    render(<App />);
+  expect(await screen.findByText("Welcome to my search engine!")).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -15,20 +15,20 @@ function App() {
 
     return (
         <html lang="en">
-        <body className="flex flex-col items-center justify-center bg-gray-100 pt-32">
-            <div className="text-center p-8 bg-white shadow-md rounded-md max-w-md w-full">
-                <h1 className="text-2xl font-bold mb-4 text-gray-800">Welcome to my search engine!</h1>
-                <h2 className="text-lg mb-6 text-gray-600">Search for a term</h2>
-                <Search
-                    searchTerm={searchTerm}
-                    setSearchTerm={setSearchTerm}
-                    setResults={setResults}
-                />
-            </div>
-            {results !== undefined && results.length > 0 && (
-                <ResultTable hits={results} />
-            )}
-            </body>
+            <body className="flex flex-col items-center justify-center bg-gray-100 pt-32">
+                <div className="text-center p-8 bg-white shadow-md rounded-md max-w-md w-full">
+                    <h1 className="text-2xl font-bold mb-4 text-gray-800">Welcome to my search engine!</h1>
+                    <h2 className="text-lg mb-6 text-gray-600">Search for a term</h2>
+                    <Search
+                        searchTerm={searchTerm}
+                        setSearchTerm={setSearchTerm}
+                        setResults={setResults}
+                    />
+                </div>
+                {results !== undefined && results.length > 0 && (
+                    <ResultTable hits={results} />
+                )}
+                </body>
         </html>
     );
 }
